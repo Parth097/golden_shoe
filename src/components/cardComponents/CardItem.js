6a68112232c525a50576c478fce7e2eb8aebe468
Parth097 <parth_uk2010@yahoo.co.uk>
2022-02-21 17:44:34 +0000

    On video buttons created

---
+++ src/components/cardComponents/CardItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function cardItem() {
+  return (
+    <div>cardItem</div>
+  )
+}
+
+export default cardItem
--- src/components/heroComponent/HeroSection.js
+++ src/components/heroComponent/HeroSection.js
@@ -1,15 +1,21 @@
+import { Button } from "bootstrap";
 import React from "react";
-import '../../App.css';
-import './HeroSection.css';
+import "../../App.css";
+import "./HeroSection.css";
 
 function HeroSection() {
-  return(
+  return (
     <div className="hero-container">
       <video src="/videos/shoe_video.mp4" autoPlay loop muted />
       <h1>Golden Shoe</h1>
       <p>What's Your Perfect Fit?</p>
+
+      <div class="btn-container">
+        <button class="btn-video">New</button>
+        <button class="btn-video">Sale</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HeroSection
+export default HeroSection;
